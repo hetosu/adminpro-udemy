71c43a8930ad12902e46a01b2bad325de7dfc8ae
htobio <htobio@atsistemas.com>
2019-10-15 23:39:35 +0200

    Modificaciones en los módulos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,32 +1,14 @@
-// Tengo que imortar el ROuterModule para poder usar las rutas
+// Tengo que importar el ROuterModule para poder usar las rutas
 // (Además del Routes)
 import { RouterModule, Routes } from '@angular/router';
 
-import { PagesComponent } from './pages/pages.component';
-
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './login/register.component';
-
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-
 import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
 
 
 // Para usar esto hay que importarlo en el app.modules.ts
 const appRoutes: Routes = [
-    {
-        path: '',
-        component: PagesComponent,
-        children: [
-            {path: 'dashboard', component: DashboardComponent},
-            {path: 'progress', component: ProgressComponent},
-            {path: 'graficas1', component: Graficas1Component},
-            {path: '', redirectTo: '/dashboard', pathMatch: 'full'}
-        ]
-    },
-
     {path: 'login', component: LoginComponent},
     {path: 'register', component: RegisterComponent},
     {path: '**', component: NopagefoundComponent }
